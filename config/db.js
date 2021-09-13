a8485f47fecf27878f6477b57a3e67cfe761ec31
Hugo Colin <colin.hugo.89@gmail.com>
2021-09-12 22:55:14 -0500

    Se agrego zona horara a la BD

--- config/db.js
+++ config/db.js
@@ -1,10 +1,9 @@
 import Sequelize from "sequelize";
 import dotenv from 'dotenv';
 
-dotenv.config( { path:"variables.env" } );
-
 const db = new Sequelize( process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
 
+    timezone: '-05:00',
     host: process.env.BD_HOST,
     dialect: 'mysql',
     port: process.env.BD_PORT,
